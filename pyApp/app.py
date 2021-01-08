f03ae89f04bfd7192cc918c5a8b41e0650f163c6
JoshuaAB98 <joshnash1998@gmail.com>
2021-01-08 11:44:48 +0000

    init

---
+++ pyApp/app.py
@@ -0,0 +1,54 @@
+import sys
+from functions import createHistoricalGraph, getCSVS
+from flask import Flask, render_template, url_for, request, redirect
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+import pandas_datareader as web
+
+companies = ['AAPL', 'ADBE', 'AMD', 'AMZN', 'DBX', 'DIS', 'FB', 'GOOG', 'INTC', 'KO', 'MSFT', 'NFLX', 'NKE', 'NOK', 'NVDA', 'PEP', 'SONO', 'SPOT', 'TSLA', 'ZM']
+
+app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
+
+@app.route('/stock', methods=['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        stock = request.form['stock']
+        per = request.form['per']
+        timeperiod = request.args.get('timeperiod')
+        return redirect(url_for('stock_calc', stock=stock, per=timeperiod))
+    else:
+        getCSVS()
+        return render_template('stock.html')
+
+@app.route("/history", methods=['POST', 'GET'])
+
+def history():
+    if request.method == 'POST':
+        stock = request.form['stock']
+        # per = request.form['per']
+        print(stock, file=sys.stderr)
+        createHistoricalGraph(stock)
+        return render_template('history.html')
+
+@app.route("/profit", methods=['POST', 'GET'])
+def profit():
+    return render_template('profit.html')
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pyApp/functions.py
@@ -0,0 +1,69 @@
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+from datetime import datetime
+import re
+import json
+import csv
+from io import StringIO
+from bs4 import BeautifulSoup
+import requests
+from dateutil.relativedelta import relativedelta
+plt.style.use('fivethirtyeight')
+
+url_stats = 'https://uk.finance.yahoo.com/quote/{}/key-statistics?p={}'
+companies = ['TSLA', 'MSFT', 'AAPL', 'AMZN', 'FB', 'GOOG', 'NFLX', 'SPOT', 'ZM', 'DBX', 'KO', 'DIS', 'PEP', 'ADBE', 'SONO', 'AMD', 'NVDA', 'INTC', 'NKE', 'NOK']
+stock = 'TSLA'
+
+def getCSVS():
+    #Statistics
+    response = requests.get(url_stats.format(stock, stock))
+    soup = BeautifulSoup(response.text, 'html.parser')
+    pattern = re.compile(r'\s--\sData\s--\s')
+    script_data = soup.find('script', text=pattern).contents[0]
+    start = script_data.find("context")-2
+    json_data = json.loads(script_data[start:-12])
+
+    now = datetime.datetime.today().strftime('%Y-%m-%d')
+
+    #Historical Stock Data
+    stock_url = 'https://query1.finance.yahoo.com/v7/finance/download/{}?'
+
+    params = {
+        'start' : now,
+        'interval' : '1d',
+        'range' : '5y',
+        'events' : 'history'
+    }
+
+    for s in companies:
+        response = requests.get(stock_url.format(s), params=params)
+        response.text
+
+        file = StringIO(response.text)
+        reader = csv.DictReader(file)
+        with open('./csvs/'+s+'.csv', 'w', newline='') as f:
+            fieldnames = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for row in reader:
+                # writer.writerow({'Date': datetime_to_float(datetime.strptime(row['Date'], "%Y-%m-%d")), 'Open': row['Open'], 'High': row['High'], 'Low': row['Low'], 'Close': row['Close'], 'Adj Close': row['Adj Close'], 'Volume': row['Volume']})
+                writer.writerow({'Date': row['Date'], 'Open': row['Open'], 'High': row['High'], 'Low': row['Low'], 'Close': row['Close'], 'Adj Close': row['Adj Close'], 'Volume': row['Volume']})
+
+def createHistoricalGraph(stockIn):
+    now = datetime.today().strftime('%Y-%m-%d')
+    oneYrAgo = datetime.now() - relativedelta(years=1)
+    stock = stockIn
+
+    # Get the stock quote
+    df = web.DataReader(stock, data_source='yahoo', start=oneYrAgo, end=now)
+
+    plt.figure(figsize=(16, 8))
+    plt.title('Closing Price History for '+stock)
+    plt.plot(df['Close'])
+    plt.xlabel('Date', fontsize=18)
+    plt.ylabel('Close Price USD ($)', fontsize=18)
+    plt.savefig('static/images/plot.png')
+
+def predictGraph(stockIn, perIn):
+    return "Hello"
